19039b45069e037cb893a9c86a70d357e88ad28d
Juste <juste.valentelyte@gmail.com>
2021-02-11 16:00:46 +0200

    Added barrels

---
+++ src/app/events/shared/event.model.ts
@@ -0,0 +1,24 @@
+//defines types for variables
+export interface IEvent{
+    id: number
+    name: string
+    date: Date
+    time: string
+    price: number
+    imageUrl: string
+    location?: {
+        address: string
+        city: string
+        country: string
+    },
+    onlineUrl?: string
+    sessions: ISession[]
+}
+
+export interface ISession {
+    id: number
+    name: string
+    presenter: string
+    duration: string
+    level: string
+}
